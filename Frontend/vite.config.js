176ec97cef40a637dbb0a9d30c36b2d203fbd861
Raj Kumar <rajkumarnishadmca@gmail.com>
2026-03-18 15:53:33 +0530

    import react from react

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,12 @@
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   preview: {
     host: "0.0.0.0",
     port: 10000,
-    allowedHosts: [
-      "portfolio-1-hvbd.onrender.com"
-    ]
+    allowedHosts: "all"
   }
 })
